c62eac47118b21ff85cd6ca9a851f5ff844b9c3e
lammy2021 <lammy2021@163.com>
2025-01-16 22:30:40 +0800

    修复

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath:'/',
   transpileDependencies: true,
   devServer: {
+    historyApiFallback: true,
+    open: true,
     proxy: {
       '/api': {
         target: 'http://localhost:6776',
