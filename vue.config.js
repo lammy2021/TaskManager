4233892884f275f47df8172f2b5924c9ca9a214b
lammy2021 <lammy2021@163.com>
2025-01-17 09:36:06 +0800

    修复

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
     historyApiFallback: true,
-    open: true,
+    // open: true,
     proxy: {
       '/api': {
         target: 'http://localhost:6776',
